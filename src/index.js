22090915c51801ca5207b9687bb3f1f152d59e6d
Erica Belzer <belzer49@gmail.com>
2023-05-25 10:03:24 -0500

    created new add event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", () => {
+    let shrek = document.getElementById(id = "tasks")
+    shrek.append(document.getElementById(id = "new-task-description"))
+    Event.preventDefault();
+  });
 });
